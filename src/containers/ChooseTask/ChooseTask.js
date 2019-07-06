594eb8544800c10a1cd28998bb9accd088737fbf
Hassan Ali <hassanqasem000@gmail.com>
2019-07-06 21:09:39 +0200

    building choose-task stage 2

--- src/containers/ChooseTask/ChooseTask.js
+++ src/containers/ChooseTask/ChooseTask.js
@@ -5,14 +5,25 @@ import "./ChooseTask.scss";
 const chooseTask = () => {
     return  (
         <section className="chooseTask">
-            <nav className="chooseTaskNav">
-                <ul className="list-unstyled">
-                    <NavigationItem link = "/data" exact = {true}>Start adding your personal data</NavigationItem>
-                    <NavigationItem link = "/skillsData" exact = {true}>Start adding your Skills</NavigationItem>
-                    <NavigationItem link = "/resumeData" exact = {true}>Start adding your career history</NavigationItem>
-                    <NavigationItem link = "/portfolioData" exact = {true}>Start adding your works</NavigationItem>
-                </ul>
-            </nav>
+            <div className="container">
+                <div className="justify-content-center align-items-center row">
+                    <div className="col-lg-9 row">
+                        <div className="col-lg-6">
+                            
+                        </div>
+                        <div className="col-lg-6">
+                            <nav className="chooseTaskNav">
+                                <ul className="list-unstyled">
+                                    <NavigationItem link = "/data" exact = {true}>Start adding your personal data</NavigationItem>
+                                    <NavigationItem link = "/skillsData" exact = {true}>Start adding your Skills</NavigationItem>
+                                    <NavigationItem link = "/resumeData" exact = {true}>Start adding your career history</NavigationItem>
+                                    <NavigationItem link = "/portfolioData" exact = {true}>Start adding your works</NavigationItem>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
