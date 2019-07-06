858dd824065f675368378fead6c4da14a584db7c
Hassan Ali <hassanqasem000@gmail.com>
2019-07-06 21:34:43 +0200

    building choose task stage 4

--- src/containers/ChooseTask/ChooseTask.js
+++ src/containers/ChooseTask/ChooseTask.js
@@ -8,25 +8,28 @@ const chooseTask = () => {
         <section className="chooseTask">
             <div className="container">
                 <div className="justify-content-center align-items-center row">
-                    <div className="col-lg-9 row">
-                        <div className="col-lg-6">
-                            <div className="pic">
-                                <div className="blur">
-                                    <img src = {chooseTaskImage} className="img-fluid d-block mx-auto" alt="contains peaple looking at a computer"/>
+                    <div className="col-lg-9 ">
+                        <div className="content row">
+                            <div className="col-lg-6 sp">
+                                <div className="pic">
+                                    <div className="pic">
+                                        <img src = {chooseTaskImage} className="img-fluid d-block mx-auto" alt="contains peaple looking at a computer"/>
+                                        <div className="blur"></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <nav className="chooseTaskNav">
-                                <ul className="list-unstyled">
-                                    <NavigationItem link = "/data" exact = {true}>Start adding your personal data</NavigationItem>
-                                    <NavigationItem link = "/skillsData" exact = {true}>Start adding your Skills</NavigationItem>
-                                    <NavigationItem link = "/resumeData" exact = {true}>Start adding your career history</NavigationItem>
-                                    <NavigationItem link = "/portfolioData" exact = {true}>Start adding your works</NavigationItem>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
+                            <div className="col-lg-6 sp">
+                                <nav className="chooseTaskNav">
+                                    <ul className="list-unstyled">
+                                        <NavigationItem link = "/data" exact = {true}>Start adding your personal data</NavigationItem>
+                                        <NavigationItem link = "/skillsData" exact = {true}>Start adding your Skills</NavigationItem>
+                                        <NavigationItem link = "/resumeData" exact = {true}>Start adding your career history</NavigationItem>
+                                        <NavigationItem link = "/portfolioData" exact = {true}>Start adding your works</NavigationItem>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div> {/*content*/ }
+                    </div> {/*col-lg-9*/ }
                 </div>
             </div>
         </section>
